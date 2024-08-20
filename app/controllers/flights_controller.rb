5da33906dfbebf461adaf204a65d31e2ba9c4086
hellosamm <142949327+hellosamm@users.noreply.github.com>
2024-08-19 16:53:24 -1000

    results form working

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,5 +2,14 @@ class FlightsController < ApplicationController
   def index
     @airports = Airport.all
     @flights = Flight.all
+    @flight_params = params.permit(:departure_airport_code, :arrival_airport_code, :passengers)
+    @search_results = @flights.select do |f|
+      f.departure_airport.code == @flight_params[:departure_airport_code] &&
+      f.arrival_airport.code == @flight_params[:arrival_airport_code]
+    end
+
+  
+    # @flight_params = params[]
+
   end
 end
