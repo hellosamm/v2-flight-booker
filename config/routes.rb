98f1fb641c8fed3deaffa32b985930f5efa3a682
hellosamm <142949327+hellosamm@users.noreply.github.com>
2024-08-20 10:09:08 -1000

    working on new booking form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "flights#index"
   get 'search', to: 'flights#index'
+  get 'booking_path', to: 'bookings#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
