0806a3d27fa6ebbd5714bf0731ee992bf325fa52
hellosamm <samanthageddes1@gmail.com>
2024-08-27 16:00:30 -0600

    passengers param is updated and works with #new booking and #show booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,7 +2,10 @@ class BookingsController < ApplicationController
   def new
     @flight = Flight.find(params[:flight_id])
     @booking = Booking.new(flight: @flight)
-    @booking.passengers.build
+
+    params[:passenger_count].to_i.times { @booking.passengers.build }
+
+  
   end
 
   def create
